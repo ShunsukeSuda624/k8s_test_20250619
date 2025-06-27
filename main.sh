80b40725323d026a49195268497cf85975801ebf
Shunsuke Suda <shunsuke.suda@outlook.jp>
2025-06-27 13:48:39 +0900

    modify main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# output.txtに現在日付を書き込む
+# output.txt縺ｫ迴ｾ蝨ｨ譌･莉倥ｒ譖ｸ縺崎ｾｼ繧
 date '+%Y/%m/%d' > output.txt
 
-# input.txtの内容をoutput.txtに追記
+# input.txt縺ｮ蜀�螳ｹ繧弛utput.txt縺ｫ霑ｽ險�
 cat input.txt >> output.txt
 
-# 5分（300秒）待機してから終了
+# 5蛻��ｼ�300遘抵ｼ牙ｾ�讖溘＠縺ｦ縺九ｉ邨ゆｺ�
 sleep 300
